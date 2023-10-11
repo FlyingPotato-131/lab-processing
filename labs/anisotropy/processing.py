9ade8e6b9403a0274a4aa88b7e6efb27dcf4a072
FlyingPotato_131 <maksim.kirillov.2003@gmail.com>
2023-10-11 23:04:26 +0300

    i don't know anymore

--- labs/anisotropy/processing.py
+++ labs/anisotropy/processing.py
@@ -3,7 +3,30 @@ import sys
 sys.path.append("../../")
 import csvreader
 import graphs
+import numpy as np
+import matplotlib.pyplot as plt
+import math
 
 data = csvreader.readTable('data.csv', 4, titleSize = 1)
 
-graphs.plot(data[0:, 0], data[0:, 2])
+sigma = data[0:, 2] * 10 / data[0:, 1] * 1000 / data[0:, 3] * 1000
+
+x = np.linspace(0, math.pi / 2, num = 1000)
+
+fig, ax = plt.subplots()
+
+plt.minorticks_on()
+plt.grid(True, "major", "both", color = "#888888")
+plt.grid(True, "minor", "both", linestyle = '--')
+
+plt.ylabel('нормированный предел прочности')
+plt.xlabel('угол, рад')
+
+ax.plot(data[0:, 0] / 180 * math.pi, sigma / sigma[0], '.')
+
+ax.plot(x, [0.58 / math.sqrt(0.58**2 * math.cos(x[i])**4 + math.sin(x[i])**4 + 1.12 * math.sin(x[i])**2 * math.cos(x[i])**2) for i in range(len(x))])
+
+#graphs.plot(data[0:, 0], sigma / sigma[0])
+
+plt.show()
+
--- labs/bar-stability/processing.py
+++ labs/bar-stability/processing.py
@@ -4,8 +4,14 @@ sys.path.append("../../")
 import csvreader
 import graphs
 
-data = csvreader.readTable('data3.csv', 2, titleSize = 0)
+data1 = csvreader.readTable('data-e.csv', 2, titleSize = 0)
+data2 = csvreader.readTable('data2.csv', 2, titleSize = 0)
+data3 = csvreader.readTable('data3.csv', 2, titleSize = 0)
+data4 = csvreader.readTable('data4.csv', 2, titleSize = 1)
 
-graphs.plot(data[0:, 1], data[0:, 0])
+graphs.plot(data1[0:, 1], data1[0:, 0], title = '346 - 322', ylabel = 'F, n', xlabel = 'dx, 0.1*mm')
+graphs.plot(data2[0:, 1], data2[0:, 0], title = '177 - 490', ylabel = 'F, n', xlabel = 'dx, 0.1*mm')
+graphs.plot(data3[0:, 1], data3[0:, 0], title = '240 - 430', ylabel = 'F, n', xlabel = 'dx, 0.1*mm')
+graphs.plot(data4[0:, 1], data4[0:, 0], title = '492 - 172', ylabel = 'F, n', xlabel = 'dx, 0.1*mm')
 
 #погрешность есть половина цены деления
