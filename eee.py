af3e29faf68298122ceb5f2d3c3e0b9cbe869474
FlyingPotato_131 <maksim.kirillov.2003@gmail.com>
2023-11-14 14:22:08 +0300

    fix

--- eee.py
+++
@@ -1,8 +0,0 @@
-import csvreader
-import graphs
-import numpy as np
-
-data = csvreader.readTable("labs/spectre-analysis/data/sq-100us/3000hz.csv", 2, titleSize = 3)
-np.random.shuffle(data)
-graphs.plot(data[0:, 0], data[0:, 1], plotFmt = '-')
-
