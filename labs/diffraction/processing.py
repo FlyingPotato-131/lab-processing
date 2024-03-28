48094ac0aa90b3530ce577e9fc68fdaa354a0378
FlyingPotato_131 <maksim.kirillov.2003@gmail.com>
2024-03-28 21:59:18 +0300

    added missing graph in diffraction

--- labs/diffraction/processing.py
+++ labs/diffraction/processing.py
@@ -6,6 +6,7 @@ import graphs
 import calculations
 import numpy as np
 import math
+import matplotlib.pyplot as plt
 
 data1 = csvreader.readData("data1.csv")
 k, b, dk, db = graphs.plotlsqm(data1[:, 3], np.sin(np.radians(data1[:, 0] + data1[:, 1] / 60)), title = "diffraction angle over wavelength", ylabel = "sin(φ_m)", xlabel = "λ, nm", bflag = False)
@@ -18,10 +19,21 @@ wvl = 5780
 Dright = -(data2[:, 2] * 60 + data2[:, 3] - data2[:, 5] * 60 - data2[:, 6]) / dwvl
 Dleft = (data2[:, 7] * 3600 + data2[:, 8] * 60 + data2[:, 9] - data2[:, 10] * 3600 - data2[:, 11] * 60 - data2[:, 12]) / dwvl
 Dtheor = data2[:, 0] / np.sqrt(1 / k**2 * 100 - data2[:, 0]**2 * wvl**2) * 180 / math.pi * 3600
+x = np.linspace(-np.max(data2[:, 0]), np.max(data2[:, 0]), num = 1000)
+DtheorGraph = abs(x / np.sqrt(1 / k**2 * 100 - x**2 * wvl**2) * 180 / math.pi * 3600)
 print(f"Dright = {Dright}")
 print(f"Dleft = {Dleft}")
 np.set_printoptions(precision=2)
 print(f"Dtheor = {Dtheor}")
+
+fig, ax = graphs.basePlot()
+plt.title("D over m")
+plt.xlabel("m")
+plt.ylabel("D, sec/angstrem")
+ax.scatter(data2[:, 0], Dright)
+ax.scatter(-data2[:, 0], Dleft)
+ax.plot(x, DtheorGraph)
+plt.show()
 
 data3 = csvreader.readData("data3.csv")
 phi1 = data3[:, 1] + data3[:, 2] / 60 + data3[:, 3] / 3600
