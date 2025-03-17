4d56e0c1c5df2231f7b76a8e29b9c614f722113a
FlyingPotato_131 <maksim.kirillov.2003@gmail.com>
2025-03-17 13:40:15 +0300

    plasma complete

---
+++ labs/plasma-probe/processing.py
@@ -0,0 +1,107 @@
+import sys
+
+sys.path.append("../../")
+import csvreader
+import graphs
+import calculations
+
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy
+
+e = 1.6e-19 #electron charge
+K = 1.38e-23 #boltzmann constant
+S = math.pi * 0.3e-3 * 7e-3 #probe area
+me = 9.109e-31 #electron mass
+p = 39.9966 #pressure
+
+single_data = csvreader.readData("single.csv")
+double_data = csvreader.readData("double.csv")
+
+graphs.plot(single_data[:, 0], single_data[:, 1] * 1e3, 1e-7, 1e-7 * 1e3, title = "I(V) for 1 probe", xlabel = "V, V", ylabel = "I, mA") #I(V), one probe
+
+
+ionpoints = 20
+k0, delta, dk0, db0 = graphs.lsqm(single_data[:ionpoints, 0], single_data[:ionpoints, 1], 1e-7 * np.ones(ionpoints), 1e-7 * np.ones(ionpoints))
+corrected = single_data[:, 1] - (k0 * 217 + delta) #correction for ionic current at Vf
+
+fig, ax = graphs.basePlot()
+plt.title("logarithmic for 1 probe")
+plt.xlabel("V, V")
+plt.ylabel("ln(I)")
+
+rmpoints = 27
+l = np.size(corrected)
+ax.errorbar(single_data[-rmpoints:, 0], np.log(corrected[-rmpoints:]), 1e-7 / corrected[-rmpoints:], 1e-7, fmt = '.') #log(I) over V
+k, b, dk, db = graphs.lsqm(single_data[-rmpoints+5:, 0], np.log(corrected[-rmpoints+5:]), 1e-7 * np.ones(l - rmpoints + 5), 1e-7 / corrected[-rmpoints+5:]) #approximate linear function
+ax.plot([np.min(single_data[-rmpoints:, 0]), np.max(single_data[-rmpoints:, 0])], [k * np.min(single_data[-rmpoints:, 0]) + b, k * np.max(single_data[-rmpoints:, 0]) + b]) #plot linear approximation
+
+Vf = (np.log(-delta) - b) / k #floating potential
+dVf = (-1e-7 / delta + db) / k + (np.log(-delta) - b) / k**2 * dk
+print(f"Vf = ({Vf:.1f} +- {dVf:.1f}) V")
+
+Te = 11.6e3 / k #electron temperature, 11.6e3 = e / K
+dTe = 11.6e3 / k**2 * dk
+print(f"T_e = ({Te:.0f} +- {dTe:.0f}) K")
+
+Vp = Vf - K * Te / e * 0.5 * np.log(math.pi / 2 * me / 1.67e-27) #plasma potential
+dVp = dVf - K * dTe / e * 0.5 * np.log(math.pi / 2 * me / 1.67e-27)
+print(f"Vp = ({Vp:.1f} +- {dVp:.1f}) V")
+
+ne = -(k0 * Vf + delta) * math.exp(0.5) / e / S * math.sqrt(1.67e-27 * 4 / K / Te) #electron concentration
+dne = (dk0 * Vf + db0) * math.exp(0.5) / e / S * math.sqrt(1.67e-27 * 4 / K / Te) - (k0 * Vf + delta) * math.exp(0.5) / e / S * math.sqrt(1.67e-27 * 4 / K) / Te**1.5 / 2 * dTe
+print(f"ne = ({ne * 1e-12:.0f} +- {dne * 1e-12:.0f}) 1e3/mm^3")
+
+N = p / K / Te #helium concentration
+dN = p / K / Te**2 * dTe
+print(f"N = ({N * 1e-12:.0f} +- {dN * 1e-12:.0f}) 1e3/mm^3")
+
+a = ne / N #ionization parameter
+da = dne / N + ne / N * (dN / N)
+print(f"a = ({a * 1e6:.0f} +- {da * 1e6:.0f}) 1e-6")
+
+rD = math.sqrt(K * Te / 4 / math.pi / ne) / e #Debai radius
+drD = 0.5 * math.sqrt(K / 4 / math.pi / ne / Te) / e * dTe + math.sqrt(K * Te / 4 / math.pi / ne) / e / ne**1.5 / 2 * dne
+print(f"rD = ({rD:.3f} +- {drD:.3f}) m")
+print()
+
+plt.show()
+
+fig, ax = graphs.basePlot()
+plt.title("I(V) for 2 probes")
+plt.xlabel("V, V")
+plt.ylabel("I, mA")
+
+ax.errorbar(double_data[:, 1], double_data[:, 2] * 1e3, 1e-7, 1e-7 * 1e3, fmt = '.') #plot
+
+k1, b1, dk1, db1 = graphs.lsqm(double_data[-30:, 1], double_data[-30:, 2], 1e-7 * np.ones(np.shape(double_data)[0]), 1e-7 * np.ones(np.shape(double_data)[0])) #linears
+k2, b2, dk2, db2 = graphs.lsqm(double_data[:30, 1], double_data[:30, 2], 1e-7 * np.ones(np.shape(double_data)[0]), 1e-7 * np.ones(np.shape(double_data)[0]))
+k, b, dk, db = graphs.lsqm(double_data[70:90, 1], double_data[70:90, 2], 1e-7 * np.ones(np.shape(double_data)[0]), 1e-7 * np.ones(np.shape(double_data)[0]))
+
+ax.plot([0, double_data[-1, 1]], [b1 * 1e3, k1 * double_data[-1, 1] * 1e3 + b1 * 1e3]) #plot linears
+ax.plot([double_data[0, 1], 0], [k2 * double_data[0, 1] * 1e3 + b2 * 1e3, b2 * 1e3])
+ax.plot([double_data[70, 1], double_data[90, 1]], [k * double_data[70, 1] * 1e3 + b * 1e3, k * double_data[90, 1] * 1e3 + b * 1e3])
+
+Te = e / K * (-b1 * b2 / (b1 - b2)) / k #electron temperature
+dTe = e / K * (b2**2 / (b1 - b2)**2 / k * db1 + b1**2 / (b1 - b2) / k * db2 - b1 * b2 / (b1 - b2) / k**2 * dk)
+print(f"T_e = ({Te:.0f} +- {dTe:.0f}) K")
+
+ne = 2 * b1 / e / S * math.sqrt(1.67e-27 * 4 / K / Te) #electron concentration
+dne = 2 * db1 / e / S * math.sqrt(1.67e-27 * 4 / K / Te) + 2 * b1 / e / S * math.sqrt(1.67e-27 * 4 / K) / Te**1.5 / 2 * dTe
+print(f"ne = ({ne * 1e-12:.0f} +- {dne * 1e-12:.0f}) 1e3/mm^3")
+
+N = p / K / Te #helium concentration
+dN = p / K / Te**2 * dTe
+print(f"N = ({N * 1e-12:.0f} +- {dN * 1e-12:.0f}) 1e3/mm^3")
+
+a = ne / N #ionization parameter
+da = dne / N + ne / N * (dN / N)
+print(f"a = ({a * 1e6:.0f} +- {da * 1e6:.0f}) 1e-6")
+
+rD = math.sqrt(K * Te / 4 / math.pi / ne) / e #Debai radius
+drD = 0.5 * math.sqrt(K / 4 / math.pi / ne / Te) / e * dTe + math.sqrt(K * Te / 4 / math.pi / ne) / e / ne**1.5 / 2 * dne
+print(f"rD = ({rD:.3f} +- {drD:.3f}) m")
+print()
+
+plt.show()
--- labs/shockwaves/processing.py
+++ labs/shockwaves/processing.py
@@ -13,10 +13,11 @@ from math import sqrt, pi
 #constant definitions
 N = 50 #number of points
 gamma = 7/5 #assuming air as two atom molecule
+# gamma = 5/3
 F2 = 1 #cross section of high pressure area
 F1 = 5 #cross section of low pressure area
 M5 = 1 #mach number at nozzle entrance
-Fs = np.linspace(1, 6.5, N) #cross section at shockwave in nozzle (parameter)
+Fs = np.linspace(1, 5, N) #cross section at shockwave in nozzle (parameter)
 k = 3655 #pressure sensor coefficient
 rho = 13596 #mercury density
 g0 = 9.81 #gravity acceleration
@@ -37,24 +38,33 @@ def M_eq(M, M_, F_, F): #define equation for finding M on lavalle nozzle, q(M) /
 def g(pp): #=u2/a1, equation 8.3
 	return (pp - 1) / np.sqrt((gamma * (gamma - 1) / 2) * (1 + (gamma + 1) / (gamma - 1) * pp))
 
+def g_2(pp): #=u2/a1, equation 8.3
+	return (pp - 1) / np.sqrt((7/5 * (7/5 - 1) / 2) * (1 + (7/5 + 1) / (7/5 - 1) * pp))
+
 def p21_eq(pp, h): #define equation for finding p2/p1, g(p2 / p1) = h(Fs)
 	return g(pp) - h
+
+def p21_eq2(pp, h): #define equation for finding p2/p1, g(p2 / p1) = h(Fs)
+	return g_2(pp) - h
 
 #calculate mach numbers
 M6 = sci.fsolve(M_eq, 2 * np.ones(N), args=(M5, F2, Fs)) #solve q function equation
 
-M7 = (2 + (gamma - 1) * M6) / (2 * gamma * M6 - (gamma - 1)) #shockwave mach number relation
+M7 = np.sqrt((2 + (gamma - 1) * M6**2) / (2 * gamma * M6**2 - (gamma - 1))) #shockwave mach number relation
 
 M3 = sci.fsolve(M_eq, 0.5 * np.ones(N), args=(M7, Fs, F1)) #solve q function equation
 
 #calculate speed of sound fractions
 #a4 / a1 = 1 since T4 = T1
+# a41 = sqrt(29 / 40)
+# a41 = sqrt(29 / 4)
+a41 = 1.5
 a37 = np.sqrt(tau(M3) / tau(M7)) #a3 / a7 from tau functions <=> temperature on shockwave
 a76 = np.sqrt(tau(M7) / tau(M6)) #a7 / a6, same method
 a65 = np.sqrt(tau(M6) / tau(M5)) #a6 / a5, same method
 a54 = 2 / (gamma + 1) #page 28
 
-hFs = M3 * a37 * a76 * a65 * a54 #h(Fs) = u2 / a1, g(p2/p1) = h(Fs)
+hFs = M3 * a41 * a37 * a76 * a65 * a54 #h(Fs) = u2 / a1, g(p2/p1) = h(Fs)
 
 #calculate pressure fractions
 p37 = pi(M3) / pi(M7) #p3 / p7 from pi functions
@@ -65,11 +75,14 @@ p54 = 0.279 #p5 / p4 from adiabatic flow
 fFs = p37 * p76 * p65 * p54 #f(Fs), p2/p1 = p4/p1 * f(Fs)
 
 #solve for p2/p1(p4/p1)
-p21 = sci.fsolve(p21_eq, np.ones(N), args=(hFs)) #p2/p1
+p21 = sci.fsolve(p21_eq2, np.ones(N), args=(hFs)) #p2/p1
 p41 = p21 / fFs #p4/p1
 
+gamma = 7/5
+
 data = csvreader.readData("data.csv")
-p41_exp = (data[:, 1] / 250 * 6 + 1) / (data[:, 0]*1e-3 * rho * g0 / 101330) #p4/p1 measured
+# p41_exp = (data[:, 1] / 250 * 6 + 1) / (data[:, 0]*1e-3 * rho * g0 / 101330) #p4/p1 measured
+p41_exp = (data[:, 1] / 250 * 6 * 9.81e4 + 759*133.3) / (data[:, 0]*133.3)
 dp41_exp = (1 / 250 * 6) / (data[:, 0]*1e-3 * rho * g0 / 101330) + (data[:, 1] / 250 * 6 + 1) / (data[:, 0]*1e-3 * rho * g0 / 101330)**2 * (1e-3 * rho * g0 / 101330) #p4/p1 error
 
 pdiff = data[:, 2] / k * 1e6 #p2 - p1 from pressure sensor, Pa
