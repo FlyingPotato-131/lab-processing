07dfeaab7d65303edb8e3792d72f7f18ada68ede
FlyingPotato_131 <maksim.kirillov.2003@gmail.com>
2025-10-08 14:57:13 +0300

    figured out how to read the damn files by stealing plot.py

--- labs/electron-plasma/processing.py
+++ labs/electron-plasma/processing.py
@@ -11,12 +11,41 @@ import csvreader
 import graphs
 import calculations
 
-index = ["", "", "Ar 2.5torr", "Ar 1torr", "", "Ar 5torr"]
+# import seaborn as sns
+
+def matrix(content): #code stolen from builtin plot.py
+    # r - radial
+    xRaw = content[2]
+    xArrRaw = xRaw.split()
+    xArr = []
+    for i in range(len(xArrRaw)):
+        xArr.append(float(xArrRaw[i]))
+    x = np.array(xArr)
+
+    # z and matrix
+    z = []
+    without_header = content[3:]
+    data = []
+    for line in without_header:
+        data.append(line.strip())
+
+    m = []
+    for line in data:
+        arr_raw = line.split()
+        arr = arr_raw[1:]
+        zOne = float(arr_raw[0])
+        z.append(zOne)
+        for i in range(len(arr)):
+            arr[i] = float(arr[i])
+        m.append(arr)
+    return x, np.array(z), np.array(m)
+
+index = ["", "Ar 2.5torr", "", "", "Ar 5torr", "Ar 4torr"] #index experiments
 
 for image_path in os.listdir("images"):
     # Загружаем изображение
     img = Image.open(f"images/{image_path}").convert("RGB")
-    # print(image_path)
+    print(image_path)
 
     # Переводим в массив numpy
     img_array = np.array(img)
@@ -37,14 +66,24 @@ for image_path in os.listdir("images"):
     height = 260
     diam = 25.3 #external tube diameter, mm
 
-    #open theoretical data
-    # with open(f"theor/{image_path[0]}/Power.txt") as 
+    plt.figure(figsize=(12, 6)) #create graph
+
+    #open theoretical data (if exists)
     if os.path.isdir(f"theor/{image_path[0]}"):
-        print(image_path[0])
-        dist_theor = csvreader.readTable(f"theor/{image_path[0]}/Power.txt", 251, titleSize = 3, split = ' ', enc = "cp1251")
+        with open(f"theor/{image_path[0]}/Temp.txt", encoding = "cp1251") as dist_file: #read file
+            dist_theor = dist_file.read().splitlines()
+
+        r, z_t, data = matrix(dist_theor)        
+        avgdata = data.mean(axis=1)
+        # print(data)
+
+        plt.plot(z_t * 10, avgdata / np.max(avgdata) * np.max(sum_channel_avg)) #plot converting z to mm and normalizing units
+
+    # if os.path.isdir(f"theor/{image_path[0]}"):
+        # print(image_path[0])
+        # dist_theor = csvreader.readTable(f"theor/{image_path[0]}/Power.txt", 251, titleSize = 3, split = ' ', enc = "cp1251")
 
     # Строим
-    plt.figure(figsize=(12, 6))
     plt.plot(z / height * diam, sum_channel_avg, color='black', linestyle='-', label="Среднее (R+G+B)/3")
     plt.plot(z / height * diam, gauss(z, *(params[0])), color = 'red', linestyle = '--', label = "подгон нормального распределения")
     plt.plot([params[0][1] / height * diam, params[0][1] / height * diam], [0, np.max(sum_channel_avg)], color = "violet", linestyle = "--")
